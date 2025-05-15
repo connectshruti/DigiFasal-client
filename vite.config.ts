5fbd542c1935c73d22ef923be2c9605de105bba9
shruti <shrutisharmaconnect@gmail.com>
2025-05-15 17:11:39 +0530

    keeps dist inside /client

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(async () => {
       historyApiFallback:true
     },
     build: {
-      outDir: path.resolve(import.meta.dirname, "../dist/public"),
+      outDir: "dist",
       emptyOutDir: true,
     },
   };
